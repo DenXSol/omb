defba67b185cd19afbe00535fc5cb9910f20b20d
DenXSol <denx@sosleek.io>
2026-05-03 13:50:39 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,8 @@
-{
-  "compilerOptions": {
-    "target": "es5",
-    "lib": ["dom", "dom.iterable", "esnext"],
-    "allowJs": true,
-    "skipLibCheck": true,
-    "strict": true,
-    "forceConsistentCasingInFileNames": true,
-    "noEmit": true,
-    "esModuleInterop": true,
-    "module": "esnext",
-    "moduleResolution": "bundler",
-    "resolveJsonModule": true,
-    "isolatedModules": true,
-    "jsx": "preserve",
-    "incremental": true,
-    "plugins": [
-      {
-        "name": "next"
-      }
-    ],
-    "paths": {
-      "@/*": ["./*"]
-    }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['arweave.net', 'www.arweave.net'],
   },
-  "include": ["next-env.d.ts", "**/*.ts", "**/*.tsx", ".next/types/**/*.ts"],
-  "exclude": ["node_modules"]
 }
+
+module.exports = nextConfig
